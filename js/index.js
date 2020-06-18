87a94e8be5c08d2b0920bf219ee2ea99fcbd1922
xuyuefeng@192.168.0.119:29418 <2670476383@qq.com>
2020-06-18 17:35:57 +0800

    fifth page

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	var animCls = ".-anim";
 
 	new fullpage('#fullpage', {
-		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
 		css3: false,
 		autoScrolling:true,
 		scrollHorizontally: true,
