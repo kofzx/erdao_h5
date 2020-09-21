9c2649eea610d936244172eb0e48987b55e82427
xuyuefeng@192.168.0.119:29418 <2670476383@qq.com>
2020-09-21 21:59:47 +0800

    feat: add Procfile

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-var express=require('express');
-var app=express();
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/'));
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-app.listen(3000, function() {
-    console.log('Server is running at http://127.0.0.1:3000');
-});
+app
+	.use(express.static(__dirname + '/'))
+	.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
